649f528acadfa245c34576b48c1003f059e7efbe
Lance Hannestad <lhannest@sfu.ca>
2018-11-19 11:27:34 -0800

    Fixed bug

--- kgx/cli/__init__.py
+++ kgx/cli/__init__.py
@@ -1,2 +1,2 @@
-from kgx.cli.utils import get_transformer, get_file_types, get_type
+from kgx.cli.utils import get_transformer_constructor, get_file_types, get_type
 from kgx.cli.decorators import handle_exception
